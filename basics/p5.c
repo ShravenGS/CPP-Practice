bbd48a8fda386cfa013395ba24d4b6087cc0e564
Shraven GS <gsshraven@gmail.com>
2025-09-01 16:45:09 +0530

    Create p5.c

---
+++ basics/p5.c
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=10,y=20,temp;
+    cout<<"Before Swapping "<<x<<" "<<y<<endl;
+    temp=x;
+    x=y;
+    y=temp;
+    cout<<"After Swapping "<<x<<" "<<y<<endl;
+}
