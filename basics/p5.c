fbdebddaee41d742729b5b0caddac992146af5a1
Shraven GS <gsshraven@gmail.com>
2025-09-01 16:54:35 +0530

    Update p5.c

--- basics/p5.c
+++ basics/p5.c
@@ -8,3 +8,18 @@ int main(){
     y=temp;
     cout<<"After Swapping "<<x<<" "<<y<<endl;
 }
+
+/*#include<iostream>
+using namespace std;
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main(){
+    int x=10,y=20,temp;
+    cout<<"Before Swapping "<<x<<" "<<y<<endl;
+    swap(&x,&y);
+    cout<<"After Swapping "<<x<<" "<<y<<endl;
+}*/
