0efcb2a8a8ca148c70476a2b1722ff7548ddb070
Shraven GS <gsshraven@gmail.com>
2025-09-02 15:51:20 +0530

    Create p5.cpp

---
+++ strings/p5.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){
+    string str1,str2;
+    cout<<"Enter the string1"<<endl;
+    getline(cin,str1);
+    str2=str1;//strcpy
+    cout<<"str1="<<str1<<endl;
+    cout<<"str2="<<str2<<endl;
+}
