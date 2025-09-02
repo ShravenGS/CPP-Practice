f222b7ca17b37dff15cfef900232ee734e350f45
Shraven GS <gsshraven@gmail.com>
2025-09-02 11:06:14 +0530

    Create p2.c

---
+++ boolean/p2.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool x;
+    cout<<"enter the x value..."<<endl;
+    cin>>x;
+    if(x==true){
+        cout<<"Condition is true"<<endl;
+    }
+    else{
+        cout<<"Condition is false"<<endl;
+    }
+}
