38d6984eca406d08df3678cb50e5503bb2799fa6
Shraven GS <gsshraven@gmail.com>
2025-09-11 08:59:52 +0530

    Create introduction.cpp

---
+++ reference variable/introduction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int& ref=a;
+    cout<<"a="<<a<<",ref="<<ref<<endl;
+    
+    ref=20;
+    cout<<"a="<<a<<",ref="<<ref<<endl;
+    
+    a=30;
+    cout<<"a="<<a<<",ref="<<ref<<endl;
+    
+    return 0;
+}
