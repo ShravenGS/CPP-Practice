be30e8332e6c0299550294f3d881586b74cd2c83
Shraven GS <gsshraven@gmail.com>
2025-09-19 15:28:51 +0530

    Create intro.cpp

---
+++ classes and objects/intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    int x;//data member
+};
+int main(){
+    //Non member function
+    A ob1;
+    cout<<"ob1.c="<<ob1.x<<endl;
+}
