3b9b0bb1b9f31de5d4ed632e5ea8340d7485c5a3
Shraven GS <gsshraven@gmail.com>
2025-09-19 15:34:44 +0530

    Create sizeof.cpp

---
+++ classes and objects/sizeof.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    int x;//data member but by default private class
+};
+int main(){
+    //Non member function
+    A ob1;
+    ob1.x=20;
+    cout<<"ob1.x="<<ob1.x<<endl;
+    cout<<"size="<<sizeof(ob1.x)<<endl;//size depends upon data type
+}
